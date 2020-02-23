2320e0da56ee72c7dc2e5e38ab3cc5d942599ea6
yamach28 <yamaguchi.ksh.tch@gmail.com>
2020-02-23 09:55:51 +0000

    change heroku app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'nameless-brushlands-18695.herokuapp.com'
+  host = 'rails-tutorial-keishi.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
