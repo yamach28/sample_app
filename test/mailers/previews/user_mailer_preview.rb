5f27ea04a0fd3e1932704691b4700c77b456c7e8
yamach28 <yamaguchi.ksh.tch@gmail.com>
2020-02-23 08:54:36 +0000

    done 11.2.2

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,8 +2,11 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at https://5ab11c424e08485c90a495c1c55936ad.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
